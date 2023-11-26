41eb1b94ff4c5b8c028b4b3dc5d54464a1818cb2
daaniyaal <daaniyaalmirza@gmail.com>
2023-11-26 14:36:33 -0800

    Added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 8080;
 const cadence1 = require("./data/cadence1.json");
 const main1 = require("./data/main1.json");
 const main2 = require("./data/main2.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
