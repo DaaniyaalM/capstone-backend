c1de0641045b525a3fb8a1b2e2893c700ad081e6
daaniyaal <daaniyaalmirza@gmail.com>
2023-11-26 14:03:23 -0800

    Added /data API

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const cadence1 = require("./data/cadence1.json");
+const main1 = require("./data/main1.json");
+const main2 = require("./data/main2.json");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/data", (req, res) => {
+  res.json({
+    cadence1,
+    main1,
+    main2,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Capstone backend listening on port ${port}`);
+});
